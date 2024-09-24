ecbc9336a55b145511492e3506f9fd705e55c042
hjpwyb <122347230+hjpwyb@users.noreply.github.com>
2024-09-24 22:48:27 +0800

    Update test1.py

--- scripts/aaa/test1.py
+++ scripts/aaa/test1.py
@@ -121,7 +121,7 @@ def main():
     open(CLEANED_FILE, 'w').close()
 
     # 扩展域名试错范围
-    possible_domains = [f"http://87{j}ck.cc" for j in range(10, 50)] + [f"http://83{j}ck.cc" for j in range(50, 100)]
+    possible_domains = [f"http://87{j}ck.cc" for j in range(20, 50)] + [f"http://83{j}ck.cc" for j in range(50, 100)]
     valid_domain = None
     
     for domain in possible_domains:
